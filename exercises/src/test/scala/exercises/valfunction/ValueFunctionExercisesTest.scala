e177e80bc2dbe61c70fac7f917e15f194d64ee56
tim.shen <tim.shen@farfetch.com>
2022-04-03 19:32:20 +0800

    commit value func & HOF

--- exercises/src/test/scala/exercises/valfunction/ValueFunctionExercisesTest.scala
+++ exercises/src/test/scala/exercises/valfunction/ValueFunctionExercisesTest.scala
@@ -11,20 +11,74 @@ class ValueFunctionExercisesTest extends AnyFunSuite with ScalaCheckDrivenProper
   /////////////////////////////////////////////////////
 
   // replace `ignore` by `test` to enable the test
-  ignore("selectDigits examples") {
+  test("selectDigits examples") {
     assert(selectDigits("hello4world-80") == "480")
     assert(selectDigits("welcome") == "")
   }
 
   // replace `ignore` by `test` to enable the test
-  ignore("selectDigits length is smaller") {
+  test("selectDigits length is smaller") {
     forAll { (text: String) =>
       assert(selectDigits(text).length <= text.length)
     }
   }
 
+  test("selectDigits must not have letter") {
+    forAll { (text: String) =>
+      selectDigits(text).foreach(c => assert(c.isDigit))
+    }
+  }
+
+  test("secret must have same length and all asterisks") {
+    forAll { (text: String) =>
+      assert(secret(text).length == text.length && secret(text).forall(_ == '*'))
+    }
+  }
+
+  test("secret is idempotent") {
+    forAll { (text: String) =>
+      val one = secret(text)
+      val twice = secret(one)
+      assert(one == twice)
+    }
+  }
+
+  test("isValidUsername") {
+    forAll {
+      (username: String) =>
+        assert(isValidUsername(username.reverse) == isValidUsername(username))
+        assert(isValidUsername(username.toLowerCase) == isValidUsername(username.toLowerCase))
+        assert(isValidUsername(username.toUpperCase) == isValidUsername(username.toUpperCase))
+    }
+  }
   ///////////////////////
   // Exercise 2: Point
   ///////////////////////
+  test("isPositive of Point") {
+    forAll { (x: Int, y: Int, z: Int) =>
+      assert(Point(x, y, z).isPositive == List(x, y, z).forall(_ >= 0))
+    }
+  }
 
+  test("Point isPositive") {
+    forAll { (x: Int, y: Int, z: Int) =>
+      assert(Point(x.max(0), y.max(0), z.max(0)).isPositive)
+    }
+  }
+
+  test("Point isEven") {
+    forAll { (x: Int, y: Int, z: Int) =>
+      assert(if (Point(x, y, z).isEven)
+        Point(x / 2 * 2, y / 2 * 2, z / 2 * 2) == Point(x, y, z)
+      else true)
+    }
+  }
+
+  test("Point forAll") {
+    forAll { (x: Int, y: Int, z: Int, predicate: Int => Boolean) =>
+      assert {
+        Point(x, y, z).forAll(predicate) == List(x, y, z).forall(predicate)
+      }
+    }
+  }
 }
